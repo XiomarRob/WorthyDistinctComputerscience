143f810c0321ac42f6d03001897a2595ae49a4e9
XiomarRob <157074573+XiomarRob@users.noreply.github.com>
2024-08-19 23:05:42 +0000

    Updated Hello World message to be specific - added a message

--- main.cs
+++ main.cs
@@ -2,6 +2,6 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  } Enter
+    Console.WriteLine ("Hello World! I am hungry");
+ }
 }
