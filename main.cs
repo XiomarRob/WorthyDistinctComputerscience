ae74895d1b3f4e8f539c83aa110181bb42a23e14
XiomarRob <157074573+XiomarRob@users.noreply.github.com>
2024-09-05 21:57:48 +0000

    Made the 4 var and 4 cons lines

--- main.cs
+++ main.cs
@@ -1,7 +1,17 @@
 using System;
 
 class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World! I am hungry");
- }
-}
+
+  public static void Main(string[] args) {
+
+    var fullName = "Lucinda Potter";
+    var dateofBirth = "6/24/1992";
+    var workPhone = "work 000-000-0101";
+    var cellPhone = "cell 000-000-0189";
+    
+    Console.WriteLine(fullName);
+    Console.WriteLine(dateofBirth);
+    Console.WriteLine(workPhone);
+    Console.WriteLine(cellPhone);
+  }
+  }
